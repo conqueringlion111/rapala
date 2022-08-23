1e6faa7e11c3a742906880b1609a195cd787c06f
Clubber <synhlee@gmail.com>
2022-08-23 14:59:44 -0400

    adding tools tests to verify total items displayed on page

--- src/main/java/com/rapala/pages/ToolsPage.java
+++ src/main/java/com/rapala/pages/ToolsPage.java
@@ -1,10 +1,19 @@
 package com.rapala.pages;
 
 import com.rapala.pagebase.PageBase;
+import com.rapala.utils.SeleniumMethods;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
 
 public class ToolsPage extends PageBase {
     public ToolsPage(WebDriver driver) {
         super(driver);
+    }
+
+    public void verifyTotalNumberOfItemsAvailable(By locator, String totalItems) {
+        SeleniumMethods sel = new SeleniumMethods(driver);
+        int totalCount = sel.findTotalElementCount(locator);
+        Assert.assertTrue(totalCount >= Integer.parseInt(totalItems), "the expected number of items not available*");
     }
 }
